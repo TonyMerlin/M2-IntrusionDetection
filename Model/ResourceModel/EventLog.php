97fec98ab8f4986454a85013be10b37321f72a21
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-23 14:39:59 +0100

    Create EventLog.php

---
+++ Model/ResourceModel/EventLog.php
@@ -0,0 +1,7 @@
+<?php
+namespace Merlin\IntrusionDetection\Model\ResourceModel;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+class EventLog extends AbstractDb
+{
+protected function _construct() { $this->_init('merlin_intrusion_event', 'event_id'); }
+}

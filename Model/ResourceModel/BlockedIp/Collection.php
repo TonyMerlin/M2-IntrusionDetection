013b1e9f329d81dbf30133462d6c4cc9e8111efd
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-23 15:12:58 +0100

    Update Collection.php

--- Model/ResourceModel/BlockedIp/Collection.php
+++ Model/ResourceModel/BlockedIp/Collection.php
@@ -1,10 +1,13 @@
 <?php
 namespace Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp;
+
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
 class Collection extends AbstractCollection
 {
-protected function _construct()
-{
-$this->_init(\Merlin\IntrusionDetection\Model\BlockedIp::class, \Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp::class);
+    protected function _construct()
+    {
+        $this->_init(\Merlin\IntrusionDetection\Model\BlockedIp::class, \Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp::class);
+    }
 }
-}
+

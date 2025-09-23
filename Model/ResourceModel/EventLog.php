df16e4312ed02faeb0737f432c69833715ce2248
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-23 15:12:32 +0100

    Update EventLog.php

--- Model/ResourceModel/EventLog.php
+++ Model/ResourceModel/EventLog.php
@@ -1,7 +1,12 @@
 <?php
 namespace Merlin\IntrusionDetection\Model\ResourceModel;
+
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
 class EventLog extends AbstractDb
 {
-protected function _construct() { $this->_init('merlin_intrusion_event', 'event_id'); }
+    protected function _construct()
+    {
+        $this->_init('merlin_intrusion_event', 'event_id');
+    }
 }

d375edde2430296acdb928443742f3e09a9fa870
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-23 14:19:57 +0100

    Create DetectorInterface.php

---
+++ Api/DetectorInterface.php
@@ -0,0 +1,21 @@
+<?php
+namespace Merlin\IntrusionDetection\Api;
+
+
+use Magento\Framework\App\RequestInterface;
+
+
+interface DetectorInterface
+{
+/**
+* Return [isHit, severity, details]
+* - isHit: bool
+* - severity: low|medium|high|critical
+* - details: string
+*/
+public function inspect(RequestInterface $request): array;
+
+
+/** Unique machine name of detector */
+public function getName(): string;
+}

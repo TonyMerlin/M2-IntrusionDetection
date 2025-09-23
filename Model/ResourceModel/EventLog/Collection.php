60ab4484439c85ae7c5db16c780797fd9c40746c
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-23 15:13:22 +0100

    Update Collection.php

--- Model/ResourceModel/EventLog/Collection.php
+++ Model/ResourceModel/EventLog/Collection.php
@@ -1,10 +1,13 @@
 <?php
 namespace Merlin\IntrusionDetection\Model\ResourceModel\EventLog;
+
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
 class Collection extends AbstractCollection
 {
-protected function _construct()
-{
-$this->_init(\Merlin\IntrusionDetection\Model\EventLog::class, \Merlin\IntrusionDetection\Model\ResourceModel\EventLog::class);
+    protected function _construct()
+    {
+        $this->_init(\Merlin\IntrusionDetection\Model\EventLog::class, \Merlin\IntrusionDetection\Model\ResourceModel\EventLog::class);
+    }
 }
-}
+

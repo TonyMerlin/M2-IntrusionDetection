401c8db732214368648b28f759fc9be7a182785d
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-23 15:14:03 +0100

    Update BlockedIp.php

--- Model/BlockedIp.php
+++ Model/BlockedIp.php
@@ -1,10 +1,12 @@
 <?php
 namespace Merlin\IntrusionDetection\Model;
+
 use Magento\Framework\Model\AbstractModel;
+
 class BlockedIp extends AbstractModel
 {
-protected function _construct()
-{
-$this->_init(\Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp::class);
-}
+    protected function _construct()
+    {
+        $this->_init(\Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp::class);
+    }
 }

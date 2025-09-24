fbd958f0a1f33b0dafa410ba1951fcff1d8cedd5
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-24 11:58:03 +0100

    Create Mode.php

---
+++ Model/Config/Source/Mode.php
@@ -0,0 +1,13 @@
+<?php
+namespace Merlin\IntrusionDetection\Model\Config\Source;
+use Magento\Framework\Option\ArrayInterface;
+class Mode implements ArrayInterface
+{
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'detect', 'label' => __('Detect (log only)')],
+            ['value' => 'block', 'label' => __('Block (403 & auto-block)')],
+        ];
+    }
+}

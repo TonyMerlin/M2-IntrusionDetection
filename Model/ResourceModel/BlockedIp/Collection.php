c6bfd7700c0709ebf93d0053ce53ee4aa3a4ad8e
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 15:53:43 +0100

    Update Collection.php

--- Model/ResourceModel/BlockedIp/Collection.php
+++ Model/ResourceModel/BlockedIp/Collection.php
@@ -1,13 +1,23 @@
 <?php
+declare(strict_types=1);
+
 namespace Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp;
 
+use Magento\Framework\Api\Search\AggregationInterface;
+use Magento\Framework\Api\Search\SearchResultInterface;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
-class Collection extends AbstractCollection
-{
-    protected function _construct()
-    {
+class Collection extends AbstractCollection implements SearchResultInterface {
+    protected $_idFieldName = 'block_id';
+    protected $aggregations;
+    protected function _construct(){
         $this->_init(\Merlin\IntrusionDetection\Model\BlockedIp::class, \Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp::class);
     }
+    public function getAggregations(){ return $this->aggregations; }
+    public function setAggregations($aggregations){ $this->aggregations=$aggregations; return $this; }
+    public function getSearchCriteria(){ return null; }
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null){ return $this; }
+    public function getTotalCount(){ return (int)$this->getSize(); }
+    public function setTotalCount($totalCount){ return $this; }
+    public function setItems(array $items = null){ return $this; }
 }
-

132d62598197d6f76457d97892abbd75ea8b708f
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 16:16:17 +0100

    Create Handler.php

---
+++ Logger/Handler.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Merlin\IntrusionDetection\Logger;
+use Magento\Framework\Logger\Handler\Base as BaseHandler;
+use Monolog\Logger;
+class Handler extends BaseHandler {
+    protected $fileName = '/var/log/merlin_ids.log';
+    protected $loggerType = Logger::DEBUG;
+}

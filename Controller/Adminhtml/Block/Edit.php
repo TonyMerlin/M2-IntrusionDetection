dc31c16ec410d1d015c7fbd65da7d04a863c64d5
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 15:38:22 +0100

    Update Edit.php

--- Controller/Adminhtml/Block/Edit.php
+++ Controller/Adminhtml/Block/Edit.php
@@ -1,12 +1,19 @@
 <?php
+declare(strict_types=1);
+
 namespace Merlin\IntrusionDetection\Controller\Adminhtml\Block;
+
 use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
 use Merlin\IntrusionDetection\Model\BlockedIpFactory;
-class Edit extends Action
-{
+
+class Edit extends Action {
     const ADMIN_RESOURCE = 'Merlin_IntrusionDetection::blocks';
-    public function __construct(Context $context, private PageFactory $resultPageFactory, private BlockedIpFactory $factory){ parent::__construct($context); }
-    public function execute(){ $id=(int)$this->getRequest()->getParam('block_id'); if($id){ $m=$this->factory->create()->load($id); if(!$m->getId()){ $this->messageManager->addErrorMessage(__('This block record no longer exists.')); return $this->_redirect('*/*/'); } } $p=$this->resultPageFactory->create(); $p->getConfig()->getTitle()->prepend($id?__('Edit Blocked IP'):__('New Blocked IP')); return $p; }
+    private $pageFactory; private $factory;
+    public function __construct(Action\Context $context, PageFactory $pageFactory, BlockedIpFactory $factory){ parent::__construct($context); $this->pageFactory=$pageFactory; $this->factory=$factory; }
+    public function execute(){
+        $id=(int)$this->getRequest()->getParam('block_id');
+        if($id){ $m=$this->factory->create()->load($id); if(!$m->getId()){ $this->messageManager->addErrorMessage(__('This record no longer exists.')); return $this->_redirect('*/*/'); } }
+        $p=$this->pageFactory->create(); $p->getConfig()->getTitle()->prepend($id?__('Edit Blocked IP'):__('New Blocked IP')); return $p;
+    }
 }

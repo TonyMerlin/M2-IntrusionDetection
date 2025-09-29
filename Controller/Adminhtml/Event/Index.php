e53b414fc1cdb043fe6d0de28a89195b2ae3f0ab
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 15:40:37 +0100

    Update Index.php

--- Controller/Adminhtml/Event/Index.php
+++ Controller/Adminhtml/Event/Index.php
@@ -1,11 +1,14 @@
 <?php
+declare(strict_types=1);
+
 namespace Merlin\IntrusionDetection\Controller\Adminhtml\Event;
+
 use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
-class Index extends Action
-{
+
+class Index extends Action {
     const ADMIN_RESOURCE = 'Merlin_IntrusionDetection::events';
-    public function __construct(Context $context, private PageFactory $resultPageFactory){ parent::__construct($context); }
-    public function execute(){ $p=$this->resultPageFactory->create(); $p->setActiveMenu('Merlin_IntrusionDetection::events'); $p->getConfig()->getTitle()->prepend(__('Intrusion Events')); return $p; }
+    private $pageFactory;
+    public function __construct(Action\Context $context, PageFactory $pageFactory){ parent::__construct($context); $this->pageFactory=$pageFactory; }
+    public function execute(){ $p=$this->pageFactory->create(); $p->setActiveMenu('Merlin_IntrusionDetection::events'); $p->getConfig()->getTitle()->prepend(__('Intrusion Events')); return $p; }
 }

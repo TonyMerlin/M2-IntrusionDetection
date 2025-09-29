53024bde436901943c220abb80c7f7b461f15dd2
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 15:36:47 +0100

    Update UnblockIpCommand.php

--- Console/Command/UnblockIpCommand.php
+++ Console/Command/UnblockIpCommand.php
@@ -1,27 +1,25 @@
 <?php
-namespace Merlin\IntrusionDetection\Console;
+declare(strict_types=1);
+
+namespace Merlin\IntrusionDetection\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Merlin\IntrusionDetection\Model\BlockService;
+use Merlin\IntrusionDetection\Api\BlockServiceInterface;
 
-class UnblockIpCommand extends Command
-{
-    protected static $defaultName = 'merlin:id:unblock-ip';
-    public function __construct(private BlockService $svc, string $name = null) { parent::__construct($name); }
-
-    protected function configure()
-    {
-        $this->setDescription('Unblock an IP')
+class UnblockIpCommand extends Command {
+    private $svc;
+    public function __construct(BlockServiceInterface $svc){ $this->svc=$svc; parent::__construct(); }
+    protected function configure(){
+        $this->setName('merlin:ids:unblock')->setDescription('Unblock an IP')
             ->addArgument('ip', InputArgument::REQUIRED);
+        parent::configure();
     }
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $n = $this->svc->unblock($input->getArgument('ip'));
-        $output->writeln("<info>Removed $n block record(s)</info>");
-        return Command::SUCCESS;
+    protected function execute(InputInterface $in, OutputInterface $out): int {
+        $ip=(string)$in->getArgument('ip'); $this->svc->unblock($ip);
+        $out->writeln("<info>Unblocked {$ip}.</info>"); return Command::SUCCESS;
     }
 }
+

a03d9cfda90f82ac56c6d0560ad02325387c3e3c
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 15:58:07 +0100

    Update EventLog.php

--- Model/EventLog.php
+++ Model/EventLog.php
@@ -1,12 +1,10 @@
 <?php
+declare(strict_types=1);
+
 namespace Merlin\IntrusionDetection\Model;
 
 use Magento\Framework\Model\AbstractModel;
 
-class EventLog extends AbstractModel
-{
-    protected function _construct()
-    {
-        $this->_init(\Merlin\IntrusionDetection\Model\ResourceModel\EventLog::class);
-    }
+class EventLog extends AbstractModel {
+    protected function _construct(){ $this->_init(\Merlin\IntrusionDetection\Model\ResourceModel\EventLog::class); }
 }

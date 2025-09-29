63bb93694b39542e33ad008be29ae3f3f03d97ba
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 15:37:48 +0100

    Update Delete.php

--- Controller/Adminhtml/Block/Delete.php
+++ Controller/Adminhtml/Block/Delete.php
@@ -1,22 +1,19 @@
 <?php
+declare(strict_types=1);
+
 namespace Merlin\IntrusionDetection\Controller\Adminhtml\Block;
+
 use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
-use Merlin\IntrusionDetection\Model\BlockedIpFactory;
 use Magento\Framework\Controller\ResultFactory;
-class Delete extends Action
-{
+use Merlin\IntrusionDetection\Model\BlockedIpFactory;
+
+class Delete extends Action {
     const ADMIN_RESOURCE = 'Merlin_IntrusionDetection::blocks';
-    public function __construct(Context $context, private BlockedIpFactory $factory){ parent::__construct($context); }
+    private $factory;
+    public function __construct(Action\Context $context, BlockedIpFactory $factory){ parent::__construct($context); $this->factory=$factory; }
     public function execute(){
         $id=(int)$this->getRequest()->getParam('block_id');
-        if($id){
-            $m=$this->factory->create()->load($id);
-            if($m->getId()){
-                try{ $m->delete(); $this->messageManager->addSuccessMessage(__('The block was deleted.')); }
-                catch(\Exception $e){ $this->messageManager->addErrorMessage($e->getMessage()); }
-            }
-        }
+        if($id){ $m=$this->factory->create()->load($id); if($m->getId()){ try{ $m->delete(); $this->messageManager->addSuccessMessage(__('Deleted.')); } catch(\Exception $e){ $this->messageManager->addErrorMessage($e->getMessage()); } } }
         return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setPath('*/*/');
     }
 }

4bbbe904fc0102d0b784fb87d7bf83eb67005663
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-09-29 15:36:14 +0100

    Update ListBlockedCommand.php

--- Console/Command/ListBlockedCommand.php
+++ Console/Command/ListBlockedCommand.php
@@ -1,27 +1,21 @@
 <?php
-namespace Merlin\IntrusionDetection\Console;
+declare(strict_types=1);
+
+namespace Merlin\IntrusionDetection\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp\CollectionFactory;
+use Merlin\IntrusionDetection\Api\BlockServiceInterface;
 
-class ListBlockedCommand extends Command
-{
-    protected static $defaultName = 'merlin:id:list-blocked';
-    public function __construct(private CollectionFactory $factory, string $name = null) { parent::__construct($name); }
-
-    protected function configure()
-    {
-        $this->setDescription('List blocked IPs');
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $col = $this->factory->create();
-        foreach ($col as $item) {
-            $output->writeln(sprintf("%s\t%s\t%s", $item->getData('ip'), $item->getData('reason'), $item->getData('expires_at') ?: 'permanent'));
-        }
+class ListBlocksCommand extends Command {
+    private $svc;
+    public function __construct(BlockServiceInterface $svc){ $this->svc=$svc; parent::__construct(); }
+    protected function configure(){ $this->setName('merlin:ids:list-blocks')->setDescription('List blocked IPs'); parent::configure(); }
+    protected function execute(InputInterface $in, OutputInterface $out): int {
+        $rows = $this->svc->listBlocks();
+        if (!$rows){ $out->writeln('<comment>No blocks.</comment>'); return Command::SUCCESS; }
+        foreach ($rows as $r) { $out->writeln(sprintf('%-15s | %-19s | %s', $r['ip'], (string)$r['expires_at'], (string)$r['reason'])); }
         return Command::SUCCESS;
     }
 }

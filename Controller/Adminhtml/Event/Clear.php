58f899fa781c386a97ca20f863c034d18eeefe65
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-10-01 12:22:25 +0100

    Update Clear.php

--- Controller/Adminhtml/Event/Clear.php
+++ Controller/Adminhtml/Event/Clear.php
@@ -5,25 +5,30 @@ namespace Merlin\IntrusionDetection\Controller\Adminhtml\Event;
 
 use Magento\Backend\App\Action;
 use Magento\Framework\App\ResourceConnection;
-use Magento\Framework\Controller\Result\RedirectFactory;
 
 class Clear extends Action
 {
     public const ADMIN_RESOURCE = 'Merlin_IntrusionDetection::events';
 
+    /** @var ResourceConnection */
+    private $rc;
+
     public function __construct(
         Action\Context $context,
-        private readonly ResourceConnection $rc,
-        private readonly RedirectFactory $resultRedirectFactory
+        ResourceConnection $rc
     ) {
         parent::__construct($context);
+        $this->rc = $rc;
     }
 
     public function execute()
     {
         $conn = $this->rc->getConnection();
         $conn->truncateTable($this->rc->getTableName('merlin_intrusion_event'));
+
         $this->messageManager->addSuccessMessage(__('Intrusion log cleared.'));
+
+        // Use parent-provided factory; do NOT redeclare as promoted/readonly property
         return $this->resultRedirectFactory->create()->setPath('*/*/');
     }
 }

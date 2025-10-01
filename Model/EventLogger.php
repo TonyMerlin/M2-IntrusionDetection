90e01c79c19633f52d49854ef441f12dc2455f0d
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-10-01 15:33:38 +0100

    Update EventLogger.php

--- Model/EventLogger.php
+++ Model/EventLogger.php
@@ -6,12 +6,7 @@ class EventLogger {
     public function __construct(\Merlin\IntrusionDetection\Model\EventLogFactory $factory, \Merlin\IntrusionDetection\Model\ResourceModel\EventLog $resource){ $this->factory=$factory; $this->resource=$resource; }
     public function log(string $detector, string $severity, string $ip, string $path, string $ua = null, ?string $details = null): void {
         $m = $this->factory->create();
-        $m->setData(['detector'=>$detector,
-         'severity'=>$severity,
-         'ip'=>$ip,
-         'path'=>$path,
-         'user_agent'=>$ua,
-         'details'=>$details]);
+        $m->setData(['detector'=>$detector,'severity'=>$severity,'ip'=>$ip,'path'=>$path,'user_agent'=>$ua,'details'=>$details]);
         try { $this->resource->save($m); } catch (\Exception $e) {}
     }
 }

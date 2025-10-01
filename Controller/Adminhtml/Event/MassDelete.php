6eae21fcf6b1d60170f514dfdfdf65c24fb177d7
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-10-01 15:29:09 +0100

    Update MassDelete.php

--- Controller/Adminhtml/Event/MassDelete.php
+++ Controller/Adminhtml/Event/MassDelete.php
@@ -1,15 +1,38 @@
 <?php
-namespace Merlin\IntrusionDetection\Controller\Adminhtml\Event;
+declare(strict_types=1);
+
+namespace Merlin\IntrusionDetection\Controller\Adminhtml\Block;
 
 use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
-use Magento\Ui\Component\MassAction\Filter;
-use Merlin\IntrusionDetection\Model\ResourceModel\EventLog\CollectionFactory;
-use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\ResourceConnection;
 
 class MassDelete extends Action
 {
-    const ADMIN_RESOURCE = 'Merlin_IntrusionDetection::events';
-    public function __construct(Context $context, private Filter $filter, private CollectionFactory $factory){ parent::__construct($context); }
-    public function execute(){ $c=$this->filter->getCollection($this->factory->create()); $n=0; foreach($c as $i){ $i->delete(); $n++; } $this->messageManager->addSuccessMessage(__('%1 event(s) were deleted.', $n)); return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setPath('*/*/'); }
+    public const ADMIN_RESOURCE = 'Merlin_IntrusionDetection::blocks';
+
+    /** @var ResourceConnection */
+    private $rc;
+
+    public function __construct(
+        Action\Context $context,
+        ResourceConnection $rc
+    ) {
+        parent::__construct($context);
+        $this->rc = $rc;
+    }
+
+    public function execute()
+    {
+        $ids = (array)($this->getRequest()->getParam('selected') ?? []);
+        $table = $this->rc->getTableName('merlin_blocked_ip');
+
+        if ($ids) {
+            $this->rc->getConnection()->delete($table, ['block_id IN (?)' => $ids]);
+            $this->messageManager->addSuccessMessage(__('Deleted %1 record(s).', count($ids)));
+        } else {
+            $this->messageManager->addNoticeMessage(__('No records selected.'));
+        }
+
+        return $this->resultRedirectFactory->create()->setPath('*/*/');
+    }
 }
